17fe250e54d34a21abb9cb36576359464b8d41f7
meaganmg <meaganmg@hotmail.com>
2024-04-12 11:19:48 +0800

    Update home-page.php
    edited image sections with proper coding to php

--- home-page.php
+++ home-page.php
@@ -34,21 +34,19 @@
 </div>
 
 <div class="col-md-4">
-    <img class="img-fluid" src="http://170.187.231.66/~mesh1/wp-content/themes/starter_for_josh_site/images/port2.jpg">
-    <h4 class="article-title">A PORTRAIT</h4>
-    <p class="date">Friday 12 December 2020</p>
-    <p class="article-p">STS has never been shy of seeking new terrains of investigation. More and more STS scholars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts.
-    </p>
-    <a class="readmore" href="#">CONTINUE READING</a>
+<img class="img-fluid" src="<?php the_field('articleimage2');?>" alt="Robot">
+<h4 class="article-title"><?php the_field('articletitle2'); ?></h4>
+<p class="date" ><?php the_field('articledate2'); ?></p>
+<p class="article-p"><?php the_field('articleexcerpt2'); ?> </p>
+<a href="<?php the_field('readmore'); ?>" class="readmore">CONTINUE READING</a>
 </div>
 
 <div class="col-md-4">
-    <img class="img-fluid" src="http://170.187.231.66/~mesh1/wp-content/themes/starter_for_josh_site/images/port3.jpg">
-    <h4 class="article-title">A PORTRAIT</h4>
-    <p class="date">Friday 12 December 2020</p>
-    <p class="article-p">STS has never been shy of seeking new terrains of investigation. More and more STS scholars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts.
-    </p>
-    <a class="readmore" href="#">CONTINUE READING</a>
+<img class="img-fluid" src="<?php the_field('articleimage3');?>" alt="Map">
+<h4 class="article-title"><?php the_field('articletitle3'); ?></h4>
+<p class="date" ><?php the_field('articledate3'); ?></p>
+<p class="article-p"><?php the_field('articleexcerpt3'); ?> </p>
+<a href="<?php the_field('readmore'); ?>" class="readmore">CONTINUE READING</a>
 </div>
 
 <div class="col-md-4">
