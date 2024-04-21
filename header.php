a29fd3fa955794fcea2353ae6dd4bbb2c5d7b1ba
meaganmg <meaganmg@hotmail.com>
2024-04-21 12:40:23 +0800

    Update header.php
    favicon

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
 <meta charset="<?php bloginfo('charset'); ?>"> 
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="icon" type="image/x-icon" href="images/favicon.ico" />
 <meta name="Description" content="Enter your description here">
 
 
